84fb1762903ce337dcbceea1f8a1362f069c1b15
JustLuck <91342911+Just-Luck@users.noreply.github.com>
2023-01-09 14:58:49 +0500

    Задача
    Напишите функцию, которая в заданной строке, состоящей из слов и знаков препинания, проверит, что все слова одинаковой длины

--- 0.19.cpp
+++ 0.19.cpp
@@ -1,3 +1,7 @@
+/*
+Напишите функцию, которая в заданной строке, состоящей из слов и знаков препинания, проверит, что все слова одинаковой длины
+*/
+
 #include <iostream>
 #include <cmath>
 using namespace std;
@@ -5,15 +9,19 @@ using namespace std;
 bool check(char* str, int n){
     int counter = 0;
     int index = 0;
-    int array[n] = {};
+    int *array = new int[n]{};
     for (int i = 0;i<n;++i){
         if (str[i] == ' ' || str[i] == '\0'){
             array[index] = counter;
-            if(index > 0 && array[index] != array[index - 1]) return false;
+            if(index > 0 && array[index] != array[index - 1]){
+                delete[] array;
+                return false;
+            }
             ++index;
             counter = 0; 
         } else counter++;
     }
+    delete[] array;
     return true;
 }
 
